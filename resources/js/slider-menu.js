7bcafd552fe28a994d4ed5b53755b415e9a0d154
Ludwig Nöjd <ludwig.nojd@hotmail.se>
2023-08-23 17:11:20 +0200

    menu stuff

--- resources/js/slider-menu.js
+++ resources/js/slider-menu.js
@@ -1,6 +1,7 @@
 const burgerButton = document.querySelector('.burger-menu')
 const burgerMenu = document.querySelector('.mobile-menu-dfs')
 const menuContents = document.querySelectorAll('.menu-content')
+const backdrop = document.querySelector('.backdrop')
 
 let toggle = false
 
@@ -9,8 +10,14 @@ burgerButton.addEventListener('click', () => {
   toggleMenu()
 })
 
+backdrop.addEventListener('click', () => {
+  toggle = !toggle
+  toggleMenu()
+})
+
 burgerMenu.style.width = "0px"
 burgerMenu.style.transition = "width 0.5s ease-out"
+backdrop.style.display = 'none'
 
 menuContents.forEach((menuContent) => {
   menuContent.style.visibility = 'hidden'
@@ -23,7 +30,7 @@ const toggleMenu = () => {
     burgerMenu.style.width = "90vw"
     burgerMenu.style.zIndex = "3"
     burgerMenu.style.transition = "width 0.3s ease-out"
-
+    backdrop.style.display = 'block'
     menuContents.forEach((menuContent) => {
       menuContent.style.visibility = 'visible'
       menuContent.style.transition = 'visibility 0.2s ease-out'
@@ -36,6 +43,7 @@ const toggleMenu = () => {
       menuContent.style.transition = 'visibility 0.2s ease-out'
     });
     burgerMenu.style.transition = "width 0.3s ease-out"
+    backdrop.style.display = 'none'
 
   }
 }
